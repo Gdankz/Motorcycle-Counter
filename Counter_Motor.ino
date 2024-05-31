1d159dc0e9540918f9884bb59ad61a8d0258f866
Gabriel Bayu <gabrielbayu03@gmail.com>
2024-05-31 14:13:52 +0700

    Update Project Counter Motorcycle

--- Counter_Motor.ino
+++ Counter_Motor.ino
@@ -1,75 +1,95 @@
-#define trigpin 7
-#define echopin 6
-int led1 = A0;
-int led2 = A1;
-int led3 = A2;
-int led4 = A3;
-int led5 = A4;
-int Buzzer = A5;
+#define trigpin 8
+#define echopin 9
+#define trig2 10 // Trig pin Ultra 2
+#define echo2 11
+int led1 = 12; // Kuning
+int led2 = 13; // Merah
 
 int objectCount = 0;
-bool objectDetected = false;
+bool objectDetected1 = false;
+bool objectDetected2 = false;
 
 void setup() {
 Serial.begin(9600);
 pinMode(trigpin, OUTPUT);
+pinMode(trig2, OUTPUT);
+pinMode(echo2, INPUT);
 pinMode(echopin, INPUT);
 pinMode(led1, OUTPUT);
 pinMode(led2, OUTPUT);
-pinMode(led3, OUTPUT);
-pinMode(led4, OUTPUT);
-pinMode(led5, OUTPUT);
-pinMode(Buzzer, OUTPUT);
 
 digitalWrite(led1, LOW);
 digitalWrite(led2, LOW);
-digitalWrite(led3, LOW);
-digitalWrite(led4, LOW);
-digitalWrite(led5, LOW);
-digitalWrite(Buzzer, LOW);
-delay(1000);
+
+delay(2000);
 }
 
 void loop() {
-  long duration, distance;
+  long duration1, distance1;
+
+  // Sensor 1
   digitalWrite(trigpin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigpin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigpin, LOW);
 
-  duration = pulseIn(echopin, HIGH);
-  distance = (duration / 2) / 29.1;
-  
-  if (distance < 20){
-    if (!objectDetected){
+  duration1 = pulseIn(echopin, HIGH);
+  distance1 = (duration1 / 2) / 29.1;
+
+  // Sensor 2
+  long duration2, distance2;
+  digitalWrite(trig2, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig2, LOW);
+
+  duration2 = pulseIn(echo2, HIGH);
+  distance2 = (duration2 / 2) / 29.1;
+
+  bool currentDetected1 = (distance1 < 20);
+  bool currentDetected2 = (distance2 , 20);
+
+  if (!objectDetected1 or objectDetected2 && currentDetected1 && currentDetected2 && objectCount < 5) {
       objectCount++;
-      objectDetected = true;
-
-      updateLEDs(objectCount);
-
-      if(objectCount >= 5){
-        digitalWrite(Buzzer, HIGH);
-        delay(1000);
-        digitalWrite(Buzzer, LOW);
-      }
+      objectDetected1 = true;
+      objectDetected2 = true;
     }
-  } else {
-      objectDetected = false;
-    }
-    Serial.print("Distance: ");
-    Serial.print(distance);
-    Serial.print(" cm");
-    Serial.print(" | Object Counted : ");
-    Serial.print(objectCount);
 
-    delay(500);
+  if (!currentDetection1 && !currentDetection2 && (objectDetected1 or objectDetected2)) {
+    if (objectCount > 0) {
+      objectCount--;
+    }
+    objectDetected1 = false;
+    objectDetected2 = false;
+  }
+    updateLEDs(objectCount);
+
+    if (objectCount >= 5) {
+      digitalWrite(led2, HIGH);
+    }
+  }
+
+  Serial.print("Distance1: ");
+  Serial.print(distance1);
+  Serial.print(" cm, Distance2: ");
+  Serial.print(distance2);
+  Serial.print(" cm | Object Counted: ");
+  Serial.println(objectCount);
+
+  delay(2000);
 }
 
-void updateLEDs(int count){
-  digitalWrite(led1, count >= 1 ? HIGH : LOW);
-  digitalWrite(led2, count >= 2 ? HIGH : LOW);
-  digitalWrite(led3, count >= 3 ? HIGH : LOW);
-  digitalWrite(led4, count >= 4 ? HIGH : LOW);
-  digitalWrite(led5, count >= 5 ? HIGH : LOW);
+void updateLEDs(int count) {
+  if (count >= 5) {
+    digitalWrite(led2, HIGH); // Nyalakan LED2
+    digitalWrite(led1, LOW);  // Matikan LED1
+  } else if (count == 4) {
+    digitalWrite(led1, HIGH); // Nyalakan LED1 jika count >= 4
+    digitalWrite(led2, LOW);  // Matikan LED2
+  } else {
+    digitalWrite(led1, LOW);  // Matikan LED1 jika count kurang dari 4
+    digitalWrite(led2, LOW);  // Matikan LED2 jika count kurang dari 4
+  }
 }
